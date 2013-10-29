af05bfa2e27d4e0857b691d5306b5e315bd2799e
MrrGingerNinja <maddginger@gmail.com>
2013-10-29 22:57:21 +0000

    Code for TCP's Tutorials 7-10

--- src/com/mgstudios/game/core/Game.java
+++ src/com/mgstudios/game/core/Game.java
@@ -5,49 +5,59 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 
+import com.mgstudios.game.graphics.Screen;
+
 public class Game extends Canvas implements Runnable {
-	public	static	final			long		serialVersionUID	= 1L;
+	public static final long serialVersionUID = 1L;
+
+	public static int width = 300;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
+
+	private Thread thread;
+	private boolean running = false;
+
+	private JFrame frame;
+
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 	
-	public	static					int			width				= 300;
-	public	static					int			height				= width / 16 * 9;
-	public	static					int			scale				= 3;
-	
-	private							Thread		thread;
-	private							boolean		running				= false;
-	
-	private							JFrame		frame;
+	private Screen screen;
 	
 	public Game() {
+		screen = new Screen(width, height);
 		frame = new JFrame();
 		
 		Dimension size = new Dimension((width * scale), (height * scale));
 		setPreferredSize(size);
-		
+
 		frame.setResizable(false);
 		frame.setTitle("2D Game");
 		frame.add(this);
 		frame.pack();
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
+
 		start();
 	}
-	
+
 	public synchronized void start() {
-		if(running) return;
+		if (running) return;
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
-	
+
 	public synchronized void stop() {
-		if(!running) return;
+		if (!running) return;
 		running = false;
 		try {
 			thread.join();
@@ -55,34 +65,42 @@ public class Game extends Canvas implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run() {
 		manageAppearance();
-		
-		while(running) {
+
+		while (running) {
 			update();
 			render();
 		}
 	}
-	
+
 	private void update() {
-		
+
 	}
-	
+
 	private void render() {
 		BufferStrategy bs = getBufferStrategy();
-		if(bs == null) {
+		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
 		
+		screen.clear();
+		screen.render();
+		
+		for(int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+
 		Graphics g = bs.getDrawGraphics();
-		g.setColor(Color.RED);
+		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
-	
+
 	private void manageAppearance() {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
